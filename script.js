603c45813e8d6f676be469c144ed9dcb21fff2b8
FrediFasber242 <novodran.1123@gmail.com>
2023-10-02 17:48:10 +0300

    Я добавил свой скрипт

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Привет!Ты попал на мой самый лучший сайт")
+var years = prompt("Сколько вам лет?")
+var name = prompt ("Как вас зовут?")
+var isAdmi = confirm("Эй какашка!Хочешь ли ты посмотреть мой лучший сайт?")
+if (isAdmin)
+{
+    alert("Ок заходи Jordan младший")
+    alert("Ваше имя " + name)
+    alert("Тебе "+ years + " Лет!")
+}
+else
+{
+    window.close()
+}
